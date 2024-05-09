ea91634a855babb4614d76232ad240d48243e930
MichalZdziubany <127780743+MichalZdziubany@users.noreply.github.com>
2024-05-09 19:13:27 +0100

    Search Page done

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ApiService } from '../services/api.service';
+import { RouterLink } from '@angular/router';
 import { IonContent, IonHeader, IonTitle, 
-  IonToolbar, IonButton, IonButtons, IonBackButton} from '@ionic/angular/standalone';
+  IonToolbar, IonButton, IonButtons, IonBackButton,IonInput, IonItem, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonList} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-search',
@@ -10,13 +12,24 @@ import { IonContent, IonHeader, IonTitle,
   styleUrls: ['./search.page.scss'],
   standalone: true,
   imports: [IonContent, IonHeader, IonTitle, 
-    IonToolbar, CommonModule, FormsModule, IonButton, IonButtons,IonBackButton]
+    IonToolbar, CommonModule, FormsModule, IonButton, IonButtons,IonBackButton,IonInput, IonItem, RouterLink, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonList]
 })
 export class SearchPage implements OnInit {
-
-  constructor() { }
+  inputValue: string = "";
+  recipes: any = [];
+  isClicked: boolean = false;
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
   }
 
+  searchRecipes(){
+    this.apiService.searchRecipe(this.inputValue).subscribe((data) => {
+      this.recipes = data.meals;
+    });
+  }
+
+  toggleContent() {
+    this.isClicked = !this.isClicked;
+  }
 }
